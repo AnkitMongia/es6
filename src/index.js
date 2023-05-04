eeb40dfc16a0017edfc05f045637b49b79219cf3
Ankit Mongia <ankit.mongia@algosec.com>
2023-05-04 17:56:43 +0530

    es6 map

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import emojipedia from "./emojipedia";
+
+
+//Map -Create a new array by doing something with each item in an array.
+
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
+
+console.log(emojipedia.map((emoji)=>{
+    return emoji.meaning.substring(0,100);
+}));
